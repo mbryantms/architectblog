ce62ad9e9d30918b75d1651be2b7d775b9f15fc4
Matthew Bryant <mbryantms@gmail.com>
2021-08-17 09:25:47 -0500

    Updated quote detail page

--- config/settings/base.py
+++ config/settings/base.py
@@ -206,6 +206,7 @@ TINYMCE_DEFAULT_CONFIG = {
     "code,help,wordcount",
     "toolbar": "undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl",
     "toolbar_sticky": "true",
+    "browser_spellcheck": "true",
 }
 TINYMCE_FILEBROWSER = True
 
@@ -256,7 +257,7 @@ FILEBROWSER_VERSIONS = {
 }
 
 FILEBROWSER_DIRECTORY = "uploads/"
-DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
+DATA_UPLOAD_MAX_MEMORY_SIZE = 2048576
 
 LOGGING = {
     "version": 1,
